38945e5b580a579b8be5683fce01ce05950dbc9e
shilcode <2850542@gmail.com>
2024-12-15 10:40:00 +0100

    Add initial components and SVG assets for the application

---
+++ components/SearchBar.tsx
@@ -0,0 +1,19 @@
+"use client";
+import  { useState } from "react";
+import SearchManufacturer from "./SearchManufacturer";
+const SearchBar = () => {
+    const [manufacturer, setmanufacturer] = useState('')
+  //   const handleSearch=()={};
+  return (
+    <form className="searchbar">
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setmanufacturer={setmanufacturer}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
